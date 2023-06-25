e94fcf0bdf7eb05325ee6d1a0694ba6d75a39282
Hakan <ahmethakanozturk34@gmail.com>
2023-06-26 00:46:47 +0300

    Singleton Added

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var example1 = Example.GetInstance;
+var example2 = Example.GetInstance;
+var example3 = Example.GetInstance;
+var example4 = Example.GetInstance;
+example1.Counter += 1;
+example2.Counter += 1;
+example3.Counter += 1;
+example4.Counter += 1;
+Console.WriteLine(example4.Counter);
+
+class Example
+{
+    public int Counter { get; set; }
+    private Example() => Console.WriteLine("Created!"); 
+    static Example() => GetInstance = new Example();
+
+    public static Example GetInstance;
+    
+    //public static Example GetInstance()
+    //{
+    //    if (Instance == null)
+    //        Instance = new();
+    //    return Instance;
+    //}
+}
