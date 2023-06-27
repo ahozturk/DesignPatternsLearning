1c6d5189fcaa00f583b0c1f7524de0a66eb585fc
Hakan <ahmethakanozturk34@gmail.com>
2023-06-27 08:32:18 +0300

    Multiton Pattern

---
+++ Multiton/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var a = Task.Run(() =>
+{
+    Database.GetInstance("MySQL");
+});
+var a2 = Task.Run(() =>
+{
+    Database.GetInstance("MySQL");
+});
+
+await Task.WhenAll(a, a2);
+
+Database.GetInstance("MySQL");
+Database.GetInstance("MySQL");
+class Database
+{
+    Database()
+    {
+        Console.WriteLine($"{nameof(Database)} Created!");
+    }
+
+    static Dictionary<string, Database> Instances = new();
+    static object _obj = new();
+    public static Database GetInstance(string key)
+    {
+        lock (_obj)
+        {
+
+            if (!Instances.ContainsKey(key))
+                Instances[key] = new Database();
+            return Instances[key];
+        }
+    }
+}
